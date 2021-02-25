1ead941ddc4be425e3fc103e3854e2d250763c68
vladimirskaya <63165081+vladimirskaya@users.noreply.github.com>
2021-02-26 02:13:57 +0300

    feat: rewrite part of task 18

--- scripts/script.js
+++ scripts/script.js
@@ -5,42 +5,50 @@ window.addEventListener('DOMContentLoaded', function(){
     function countTimer(deadline){
         let timeHours = document.querySelector('#timer-hours'),
             timeMinutes = document.querySelector('#timer-minutes'),
-                timeSeconds = document.querySelector('#timer-seconds');
+            timeSeconds = document.querySelector('#timer-seconds');
 
         function getTimeRemaining() {
             let dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = Math.floor(timeRemaining % 60),
-                minutes = Math.floor(timeRemaining / 60 % 60),
-                hours = Math.floor(timeRemaining /60 / 60) % 24,
-                days = Math.floor(timeRemaining /60 / 60) / 24;
-                return{ timeRemaining, hours, minutes, seconds };
+				timeData = {
+					timeRemaining, 
+					hours: 0, 
+					minutes: 0,
+					seconds: 0
+				};
+				
+			if (timeRemaining > 0){ 	
+				timeData.seconds = Math.floor(timeRemaining % 60),
+				timeData.minutes = Math.floor(timeRemaining / 60 % 60),
+				timeData.hours = Math.floor(timeRemaining /60 / 60) % 24,
+				timeData.days = Math.floor(timeRemaining /60 / 60) / 24;
+			}
+			return timeData;
         } 
         
+		function correctTimeView(timeNumber){
+			return (timeNumber < 10)? '0' + timeNumber : timeNumber;
+		}
+		
         function updateClock() {
             let timer = getTimeRemaining();
-
-            console.log(timer);
-            // console.log('seconds: ', seconds);
-            // console.log('minutes: ', minutes);
-            // console.log('hours: ', hours);
-
-            timeHours.textContent = timer.hours;
-            timeMinutes.textContent = timer.minutes;
-            timeSeconds.textContent = timer.seconds;
-
-            if (timer.timerRemaining > 0) {
-                setTimeout(updateClock, 1000);
-            }   
+            //console.log(timer);
+			if (timer.timeRemaining < 0){
+				document.querySelector('.timer-numbers').style.color = 'red';
+				clearInterval(comeOn);
+			}
+            timeHours.textContent = correctTimeView(timer.hours);
+            timeMinutes.textContent = correctTimeView(timer.minutes);
+            timeSeconds.textContent = correctTimeView(timer.seconds);           
         }
 
-        updateClock();
+		let comeOn = setInterval(updateClock, 1000);
 
     }
 
-    countTimer('31 july 2019');
-        // setInterval(countTimer, 1000,'31 july 2021');
+    countTimer('31 july 2021');
+})
 
 
         //menu
