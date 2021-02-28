5f5b79a4e338a067e99ad405b3baf2614280fa44
vladimirskaya <63165081+vladimirskaya@users.noreply.github.com>
2021-02-28 23:13:24 +0300

    feat: only 2 eventListeners |  toggleMenu

--- scripts/script.js
+++ scripts/script.js
@@ -53,17 +53,27 @@ window.addEventListener('DOMContentLoaded', function () {
     const toggleMenu = () => {
 
         const btnMenu = document.querySelector('.menu'),
-            menu = document.querySelector('menu'),
-            closeBtn = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li');
+				menu = document.querySelector('menu');
 
         const handlerMenu = function () {
             menu.classList.toggle('active-menu');
         };
+		
+		//первый обработчик, через делегирование
+	menu.addEventListener('click', (event) => {
+		let target = event.target;
+            //console.log('target1: ', target);
+            target = target.closest('a'); // найдет ближайшую ссылку
+            //console.log('target2: ', target);*/
+			if (target){
+				//console.log('зашло в условие, причем таргет сейчас: ', target);
+				handlerMenu();	
+			}
+		
+	});
+		
+		// второй обработчик без делегирования (сделать один общий для двух <- услож.)
         btnMenu.addEventListener('click', handlerMenu);
-        closeBtn.addEventListener('click', handlerMenu);
-
-        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
     };
 
     toggleMenu();
